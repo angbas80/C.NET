e28f57273c1377a584287ab718a3a9aff2e5035f
Aggelos.Basdekis <aggelos.basdekis@gmail.com>
2022-10-12 20:59:25 +0300

    HelloWorld/Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    /// <summary>
+    /// Το πρώτο μου πρόγραμμα σε C#. 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Coding Factory!");
+            Console.WriteLine(); // shortcut -- cw and double tab
+        }
+    }
+}
